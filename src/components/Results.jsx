84a26f3b88854489c31f34b018b130926d181d04
orestis <ob00042@gmail.com>
2017-11-30 18:04:38 +0200

    Final part

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Winner from './Winner';
+import * as actionCreators from '../action_creators';
 
 export class Results extends React.PureComponent {
 	getPair() {
@@ -47,4 +48,7 @@ function mapStateToProps(state) {
 	}
 }
 
-export const ResultsContainer = connect(mapStateToProps)(Results);
+export const ResultsContainer = connect(
+	mapStateToProps,
+	actionCreators
+)(Results);
--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
+import * as actionCreators from '../action_creators';
 
 export class Voting extends React.PureComponent {
   render() {
@@ -19,8 +20,12 @@ export class Voting extends React.PureComponent {
 function mapStateToProps(state) {
   return {
     pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
     winner: state.get('winner')
   };
 }
 
-export const VotingContainer = connect(mapStateToProps)(Voting);
+export const VotingContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Voting);
